47262e3c458efb506f285d9fbfe484f553cebc2d
Deepankar Senapati <deepankar066@gmail.com>
2025-11-25 17:19:33 +0530

    Apply latest project changes

--- test_save.py
+++
@@ -1,6 +0,0 @@
-# test_save.py
-from agent import save_draft, load_drafts, DRAFTS_PATH
-print("DRAFTS_PATH:", DRAFTS_PATH)
-res = save_draft(email_id="email_test", draft_text="This is a quick test draft", saved_by="you@company.com")
-print("save_draft returned:", res)
-print("Current drafts:", load_drafts())
